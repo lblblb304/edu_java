fd0ddeded80ae523bf1161150af80fc3eec14a31
PSH <lblblb304@gmail.com>
2025-05-01 21:23:37 +0900

    0501 공부 자바첫커밋

---
+++ chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,13 @@
+package sec03.exam01;
+
+/*
+작성자: 박상혁
+작성일: 2025.05.01
+ */
+public class Hello {
+	// 프로그램 실행 진입점
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");
+	} // end of main
+} // end of class

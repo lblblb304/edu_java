28ce1ee7afdeca41d084cfdaf8b39eff14c4c60c
PSH <lblblb304@gmail.com>
2025-05-02 12:43:04 +0900

    0502 오전 공부

---
+++ chap02/src/sec02/exam03/LongExample.java
@@ -0,0 +1,14 @@
+package sec02.exam03;
+
+public class LongExample {
+	public static void main(String[] args) {
+		long var1 = 10;
+		long var2 = 20L;
+		// long var3 = 1000000000000; 뒤에 L을 붙이지 않았으므로 long으로 선언되지 않았다.
+		long var4 = 1000000000000L;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var4);
+	}
+}
